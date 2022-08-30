4cb046b7858da5ccd5885128a57a5bcac802d9a2
Branden Ge <86636108+brandenge@users.noreply.github.com>
2022-08-29 22:48:26 -0500

    Added getBooks method and rendered carousel

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+import Carousel from 'react-bootstrap/Carousel';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -10,16 +12,42 @@ class BestBooks extends React.Component {
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
 
-  render() {
+  getBooks = async() => {
+    try {
+      const bookData = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/books`);
+      this.setState({
+        books: bookData.data
+      });
+    } catch (error) {
+      console.log('Error in getBooks', error);
+    }
+  };
 
+  componentDidMount() {
+    this.getBooks();
+  }
+
+  render() {
     /* TODO: render all the books in a Carousel */
 
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
         {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+          <Carousel>
+            {this.state.books.map(book => {
+              return (
+                <Carousel.Item key={book._id}>
+                  <img src ='https://source.unsplash.com/random/300x300' alt='Random' />
+                  <Carousel.Caption>
+                    <h3>{book.title}</h3>
+                    <p>{book.description}</p>
+                    <p>{book.status}</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              );
+            })}
+          </Carousel>
         ) : (
           <h3>No Books Found :(</h3>
         )}
