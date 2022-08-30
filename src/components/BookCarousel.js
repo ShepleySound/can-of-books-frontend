c8e10e9eac9b5420d1fd78099d03d67421422748
Robert Shepley <shepleysound@gmail.com>
2022-08-30 16:04:14 -0700

    Delete button tweaks.

--- src/components/BookCarousel.js
+++ src/components/BookCarousel.js
@@ -10,12 +10,14 @@ class BookCarousel extends React.Component {
         {this.props.books.map(book => {
           return (
             <Carousel.Item key={book._id}>
-              <img src ='assets/cover-unavailable-image.png' alt='Random' width={400} height={600} />
-              <Carousel.Caption>
+              <Button className='Carousel_deleteButton' variant="light" onClick={() => console.log(book)}>
+                <img className='Carousel_deleteButton_image' src='assets/icons8_trash.svg' alt='delete' width={18} height={18}></img>
+              </Button>
+              <img src ='assets/cover-unavailable-image.png' alt='Random' width={400} height={600}/>
+              <Carousel.Caption className='Carousel_caption'>
                 <h3>{book.title}</h3>
                 <p>{book.description}</p>
                 <p>{book.status}</p>
-                <Button variant="light" onClick={() => console.log(book)}>Do a thing</Button>
               </Carousel.Caption>
             </Carousel.Item>
           );
