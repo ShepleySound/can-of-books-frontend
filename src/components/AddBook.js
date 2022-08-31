54ee1e6c83f38176ee35667b2f80a910f6a1c9c6
Robert Shepley <shepleysound@gmail.com>
2022-08-30 17:11:24 -0700

    Update form logic.

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -9,34 +9,38 @@ class AddBook extends React.Component {
     super(props);
     this.state = {
       title: '',
-      author: ''
+      author: '',
+      description: '',
+
+
     }
   }
 
   handleSubmit = async (e) => {
     e.preventDefault()
-    console.log("Title:", this.state.title)
-    console.log("Author:", this.state.author)
-
     const bookSearch = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${this.state.title}+inauthor:${this.state.author}&printType=books&projection=lite&maxResults=5`)
-    console.log(bookSearch.data)
   }
 
   handleInputChange = (e) => {
     const target = e.target;
     const value = target.value;
     const name = target.name;
-
-    this.setState({
-      [name]: value,
-    })
+    if (target.type === 'checkbox') {
+      this.setState({
+        [name]: target.checked,
+      })
+    } else {
+      this.setState({
+        [name]: value,
+      })
+    }
   }
 
   render() {
     return(
       <Modal show={this.props.show} onHide={this.props.handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Public Book Search</Modal.Title>
+          <Modal.Title>Add Book to Library</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form onSubmit={this.handleSubmit}>
@@ -48,14 +52,19 @@ class AddBook extends React.Component {
               <Form.Label>Author</Form.Label>
               <Form.Control name="author" value={this.state.author} onChange={this.handleInputChange} placeholder="Enter author name..."></Form.Control>
             </Form.Group>
+            <Form.Group className='mb-3' controlId="BookSearchForm.description">
+              <Form.Label>Description</Form.Label>
+              <Form.Control as="textarea" rows={3} name="description" checked={false} onChange={this.handleInputChange} placeholder="Enter Description..."></Form.Control>
+            </Form.Group>
+            <Form.Group className='mb-3' controlId="BookSearchForm.hasRead">
+              <Form.Check type="switch" id="read-switch" label="Have you read this book?" name="hasRead" value={this.state.hasRead} onChange={this.handleInputChange}/>
+            </Form.Group>
             <Button className="mt-3" variant="dark" type="submit" onClick={this.handleSubmit}>
               Add Book
             </Button>
           </Form>
-
           </Modal.Body>
           <Modal.Footer>
-          <small>Search powered by Google Books</small>
           </Modal.Footer>
       </Modal>
     )
