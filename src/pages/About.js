062caaed4745f390a876df6bfac386fd243989e5
Robert Shepley <shepleysound@gmail.com>
2022-09-01 10:32:35 -0700

    Add source links to about page.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,12 @@
 import { Component } from "react";
+import './About.css';
 
 class About extends Component {
 
   render() {
     return(
       <main className="Main">
-
+        <h1>About</h1>
         <h1 className="text-white">About The Devs</h1>
         <div className="dev text-white">
           <h2>Branden Ge</h2>
@@ -15,6 +16,9 @@ class About extends Component {
           <h2>Robert Shepley</h2>
           <p>Software Developer - Seattle, WA</p>
         </div>
+        <h2>GitHub Source</h2>
+        <a href="https://github.com/shepleysound/can-of-books-frontend">Front-End</a>
+        <a href="https://github.com/brandenge/can-of-books-backend">Back-End</a>
       </main>
     )
   }
